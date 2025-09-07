8681ab48dacf8f68c0d1159029e5f902fce0d103
AT <akivataub1234@gmail.com>
2025-09-07 17:39:01 -0400

    Auto-commit unstaged changes: Sun Sep  7 05:39:01 PM EDT 2025

--- examples/pong/src/main.c
+++ examples/pong/src/main.c
@@ -104,12 +104,15 @@ int main(void) {
             lastscorelp = score_left_person;
             score_left_person++;
             if (score_left_person == 5) {
-                gfx_FillScreen(255);
-                gfx_SetTextScale(2, 2);
-                gfx_PrintStringXY("Right Scores!", 20, 30);
-                gfx_SetTextScale(1, 1);
-                gfx_SwapDraw();
-                delay(1500);
+            
+            } else {
+                    gfx_FillScreen(255);
+                    gfx_SetTextScale(2, 2);
+                    gfx_PrintStringXY("Right Scores!", 20, 30);
+                    gfx_SetTextScale(1, 1);
+                    gfx_SwapDraw();
+                    delay(1500);
+                }
             }
             if (score_left_person > lastscorelp) {
                 gfx_FillScreen(255);
