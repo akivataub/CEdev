fc17ee1808336e9e3f44eff541b7710b58faa150
AT <akivataub1234@gmail.com>
2025-09-07 18:41:01 -0400

    Auto-commit unstaged changes: Sun Sep  7 06:41:01 PM EDT 2025

--- examples/pong/src/main.c
+++ examples/pong/src/main.c
@@ -81,10 +81,10 @@ int endgame(void) {
             gfx_SwapDraw();
         }
         if (menuitem == 3) {
-            menuitem = 1;
+            menuitem = 0;
         }
         if (menuitem == 0) {
-            menuitem = 2;
+            menuitem = 3;
         }
     }
     gfx_End();
