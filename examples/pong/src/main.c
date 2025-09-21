a8f88639a712f97374df40e3dab30d569a708012
AT <akivataub1234@gmail.com>
2025-09-20 22:36:01 -0400

    Auto-commit unstaged changes: Sat Sep 20 10:36:01 PM EDT 2025

--- examples/pong/src/main.c
+++ examples/pong/src/main.c
@@ -7,7 +7,7 @@
 
 #define SCREEN_WIDTH 320 //x
 #define SCREEN_HEIGHT 240 //y
-
+//hi2
 int ball_x, ball_y;
 int ball_vel_x, ball_vel_y;
     
@@ -16,8 +16,12 @@ int score_right_person = 0;
 int lastscorelp = 0;
 int lastscorerp = 0;
 
+//first paddle
 int paddlex = 2;
 int paddley = 160;
+//second paddle
+int paddle2x = 312;
+int paddle2y = 160;
 
 int menuitem = 1;
 
@@ -49,6 +53,53 @@ void itoa(int num, char* str) {
 }
 
 int main(void) {
+    gfx_Begin();
+    gfx_FillScreen(255);
+    gfx_PrintStringXY("Play Again?", 20, 10);
+    gfx_PrintStringXY("> Yes", 30, 20);
+    gfx_PrintStringXY("  No", 30, 30);
+    while (1) {
+        kb_Scan();
+            if (kb_IsDown(kb_KeyDown)) {
+                menuitem = 2;
+                delay(150);
+            } else if (kb_IsDown(kb_KeyUp)) {
+                menuitem = 1;
+                delay(150);
+            }
+        if (menuitem == 1) {
+            gfx_FillScreen(255);
+            gfx_PrintStringXY("Play Again?", 20, 10);
+            gfx_PrintStringXY("> Yes", 30, 20);
+            gfx_PrintStringXY("  No", 30, 30);
+            gfx_SwapDraw();
+        }
+        if (menuitem == 2) {
+            gfx_FillScreen(255);
+            gfx_PrintStringXY("Play Again?", 20, 10);
+            gfx_PrintStringXY("  Yes", 30, 20);
+            gfx_PrintStringXY("> No", 30, 30);
+            gfx_SwapDraw();
+        }
+        if (menuitem == 3) {
+            menuitem = 1;
+        }
+        if (menuitem == 0) {
+            menuitem = 2;
+        }
+        if (menuitem == 1 && kb_IsDown(kb_KeyEnter)) {
+            gfx_FillScreen(255);
+            score_left_person = 0;
+            score_right_person = 0;
+            lastscorelp = 0;
+            lastscorerp = 0;
+            break;
+        } else if (menuitem == 2 && kb_IsDown(kb_KeyEnter)) {
+            gfx_FillScreen(255);
+            goto ending;
+        }
+    } // while (1) loop ending
+
     startgame:
 
     endg = false;
@@ -58,26 +109,42 @@ int main(void) {
     ball_vel_x = 2; // Start moving right
     ball_vel_y = 2; // Slight vertical movement
 
-    gfx_Begin();
-    //itoa(paddley, paddleystring); TEST
+    //itoa(paddley, paddleystring); TEST1
     while (!kb_IsDown(kb_KeyClear)) {
         kb_Scan();
         bool balldrawn = false;
         gfx_SetColor(0);
-            if (kb_IsDown(kb_KeyDown)) {
+            if (kb_IsDown(kb_Key2nd)) {
                 if (paddley == 220)
+                {
+                    
+                } else {
+                    paddley = paddley - 3;
+                }
+            }
+            else if (kb_IsDown(kb_KeyAlpha)) {
+                if (paddley == 0)
                 {
                     
                 } else {
                     paddley = paddley + 3;
                 }
             }
-            else if (kb_IsDown(kb_KeyUp)) {
-                if (paddley <= 0)
+            // second paddle
+            if (kb_IsDown(kb_KeyDown)) {
+                if (paddle2y == 220)
                 {
-                    
+
                 } else {
-                    paddley = paddley - 3;
+                    paddle2y = paddle2y + 3;
+                }
+            }
+            else if (kb_IsDown(kb_KeyUp)) {
+                if (paddle2y == 0)
+                {
+
+                } else {
+                    paddle2y = paddle2y - 3;
                 }
             }
 
@@ -95,12 +162,35 @@ int main(void) {
             ball_vel_x = ball_vel_x - ball_vel_x - ball_vel_x;
             //ifhit = true; TEST
         }
+        if (ball_x >= paddle2x &&  ball_x <= paddle2x + paddle_width && ball_y >= paddle2y && ball_y <= paddle2y + paddle_height) {
+            ball_vel_x = ball_vel_x - ball_vel_x - ball_vel_x;
+            //ifhit = true; TEST
+        }
 
         if (ball_y >= 238 || ball_y <= 0) {
             ball_vel_y = ball_vel_y - ball_vel_y - ball_vel_y;
         }
         if (ball_x >= 318) {
-            ball_vel_x = ball_vel_x - ball_vel_x - ball_vel_x;
+            lastscorerp = score_right_person;
+            score_right_person++;
+            if (score_right_person == 5) {
+                    gfx_FillScreen(255);
+                    gfx_SetTextScale(2, 2);
+                    gfx_PrintStringXY("P1 Wins!", 20, 30);
+                    gfx_SetTextScale(1, 1);
+                    gfx_SwapDraw();
+                    delay(1500);
+                    endg = true;
+                    goto endgame;
+            } else if (score_right_person > lastscorerp) {
+                    gfx_FillScreen(255);
+                    gfx_SetTextScale(2, 2);
+                    gfx_PrintStringXY("P1 Scores!", 20, 30);
+                    gfx_SetTextScale(1, 1);
+                    gfx_SwapDraw();
+                    delay(1500);
+                    goto startgame;
+            }
         }
 
         if (ball_x <= 2) {
@@ -109,7 +199,7 @@ int main(void) {
             if (score_left_person == 5) {
                     gfx_FillScreen(255);
                     gfx_SetTextScale(2, 2);
-                    gfx_PrintStringXY("Right Wins!", 20, 30);
+                    gfx_PrintStringXY("P2 Wins!", 20, 30);
                     gfx_SetTextScale(1, 1);
                     gfx_SwapDraw();
                     delay(1500);
@@ -118,7 +208,7 @@ int main(void) {
             } else if (score_left_person > lastscorelp) {
                     gfx_FillScreen(255);
                     gfx_SetTextScale(2, 2);
-                    gfx_PrintStringXY("Right Scores!", 20, 30);
+                    gfx_PrintStringXY("P2 Scores!", 20, 30);
                     gfx_SetTextScale(1, 1);
                     gfx_SwapDraw();
                     delay(1500);
@@ -149,13 +239,21 @@ int main(void) {
 
         //scores
         char slps[2];
-        itoa(score_left_person, slps);
-        gfx_PrintStringXY(slps ,20 ,0);
+        itoa(score_right_person, slps);
+        gfx_PrintStringXY(slps ,20 ,5);
+        gfx_PrintStringXY("P1:" ,5 ,5);
+        char srps[2];
+        itoa(score_left_person, srps);
+        gfx_PrintStringXY(srps ,300 ,5);
+        gfx_PrintStringXY("P2:" ,285 ,5);
+
+        
         
 
         //drawing ball and paddle
         gfx_FillCircle(ball_x, ball_y, 2);
         gfx_FillRectangle(paddlex, paddley, paddle_width, paddle_height);
+        gfx_FillRectangle(paddle2x, paddle2y, paddle_width, paddle_height);
 
         endgame:
         if (endg == true) {
@@ -195,7 +293,9 @@ int main(void) {
                 if (menuitem == 1 && kb_IsDown(kb_KeyEnter)) {
                     gfx_FillScreen(255);
                     score_left_person = 0;
+                    score_right_person = 0;
                     lastscorelp = 0;
+                    lastscorerp = 0;
                     break;
                 } else if (menuitem == 2 && kb_IsDown(kb_KeyEnter)) {
                     gfx_FillScreen(255);
